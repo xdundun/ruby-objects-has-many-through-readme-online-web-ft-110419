7034645477f8fc0311af0ccef27f3c4885c21404
Dardan Gjonbalaj <xdundun@gmail.com>
2019-11-20 23:53:52 +0000

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,27 @@
 class Customer
-  
-end
+  attr_accessor :name, :age
+  @@all = []
+  def initialize(name,age)
+    @name = name
+    @age = age
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_meal(waiter, total, tip)
+    Meal.new(waiter,self,total,tip)
+  end
+
+  def meals
+    Meal.all.select { |meal| meal.customer == self }
+  end
+
+  def waiters
+    #take my array of meal instances and return an array of waiter instances associated with those meals
+    meals.map { |meal| meal.waiter}
+  end
+
+end #end of class
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,27 @@
 class Waiter
+  attr_accessor :name, :yrs_experience
 
-end
+  @@all=[]
+
+  def initialize(name,yrs_experience)
+    @name = name
+    @yrs_experience = yrs_experience
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_meal(customer,total,tip)
+      Meal.new(self,customer,total,tip)
+  end
+
+  def meals
+    Meal.all.select { |meal| meal.waiter == self}
+  end
+
+  def best_tipper
+    meals.max_by { |meal| meal.tip }.customer
+  end
+end #end of class
